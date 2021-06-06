5551f1fa015a707b5e6af3855dde2c4795d18e0e
Aasson <84249592+Aasson@users.noreply.github.com>
2021-06-06 03:16:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding : utf-8 -*-
 print("Nou se CARAVANA, Inc.")
-Input input("Koman ou rele? : ")
+Input = input("Koman ou rele? : ")
 CEO = "ASSADE"
 if Input == CEO:
   print("Ou se CEO AN !")
